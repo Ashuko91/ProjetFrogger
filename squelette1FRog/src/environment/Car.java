76754b67ed996d66e609a95ece013c3b53e5a26d
dribeir <ribeirodavid209@gmail.com>
2021-11-23 17:39:44 +0100

    23/11/21 17h40

---
+++ squelette1FRog/src/environment/Car.java
@@ -0,0 +1,40 @@
+package environment;
+
+import java.awt.Color;
+
+import gameCommons.Game;
+import graphicalElements.Element;
+import util.Case;
+
+public class Car {
+	private Game game;
+	private Case leftPosition;
+	private boolean leftToRight;
+	private int length;
+	private final Color colorLtR = Color.BLACK;
+	private final Color colorRtL = Color.BLUE;
+
+	//TODO Constructeur(s)
+	public Car(Game game, Case leftPosition, boolean leftToRight) {
+		this.game = game;
+		this.leftPosition = leftPosition;
+		this.leftToRight = leftToRight;
+
+	}
+	//TODO : ajout de methodes
+
+	
+	
+	/* Fourni : addToGraphics() permettant d'ajouter un element graphique correspondant a la voiture*/
+	private void addToGraphics() {
+		for (int i = 0; i < length; i++) {
+			Color color = colorRtL;
+			if (this.leftToRight){
+				color = colorLtR;
+			}
+			game.getGraphic()
+					.add(new Element(leftPosition.absc + i, leftPosition.ord, color));
+		}
+	}
+
+}
